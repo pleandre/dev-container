5e9c594468bb655920607c31673c1404d1f8d841
Pierre LEANDRE <leandre@et.esiea.fr>
2024-06-21 08:45:36 +0800

    Add logs on C/C++ installation

--- scripts/languages/c/install-c.sh
+++ scripts/languages/c/install-c.sh
@@ -5,6 +5,7 @@ set -e
 echo "> Installing C and C++ Programming Tools"
 space_before=$(df --output=avail / --block-size=1 | tail -n 1)
 
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 apt install -qq -y build-essential \
 	make \
 	tar \
@@ -17,22 +18,27 @@ apt install -qq -y build-essential \
 	valgrind \
 	git \
 	libboost-all-dev
+echo ">> Installed dependencies: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # Install vcpkg
 echo ">> Installing Vcpkg"
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 
 cd /opt/
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.sh
 cd /scripts/
+echo ">> Installed vcpkg: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # Install conan
 echo ">> Installing Conan"
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 
 wget ${CONAN_DOWNLOAD_URL} -O /tmp/conan.deb -q
 dpkg -i /tmp/conan.deb
 rm -rf /tmp/conan.deb
+echo ">> Installed conan: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # Setup Environment Variables
 echo ">> Copy C environment variables"
