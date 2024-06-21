a2ccbab4323129b3e55c8f84e6686ef6a709d6c9
Pierre LEANDRE <leandre@et.esiea.fr>
2024-06-21 08:35:19 +0800

    Add logs on python installation

--- scripts/languages/python/install-python.sh
+++ scripts/languages/python/install-python.sh
@@ -5,16 +5,20 @@ set -e
 echo "> Installing Miniconda"
 space_before=$(df --output=avail / --block-size=1 | tail -n 1)
 
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 su -l $DEV_CONTAINER_USER /bin/bash -c "mkdir -p ~/.miniconda3"
 su -l $DEV_CONTAINER_USER /bin/bash -c "wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O  ~/.miniconda3/miniconda.sh"
 su -l $DEV_CONTAINER_USER /bin/bash -c "/bin/bash ~/.miniconda3/miniconda.sh -b -u -p ~/.miniconda3"
 rm -rf ~/.miniconda3/miniconda.sh
 su -l $DEV_CONTAINER_USER /bin/bash -c "~/.miniconda3/bin/conda init bash"
+echo ">> Setup latest miniconda.sh: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # Create conda environment with python version
 echo ">> Creating Conda environment with Python version"
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 su -l $DEV_CONTAINER_USER /bin/bash -c "source ~/.miniconda3/bin/activate base && conda create -n ${CONDA_ENV} python=${PYTHON_VERSION} -y"
 su -l $DEV_CONTAINER_USER /bin/bash -c "source ~/.miniconda3/bin/activate ${CONDA_ENV} && conda install -n ${CONDA_ENV} pip -y"
+echo ">> Created Python Environment: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # Activate this environment by default
 echo ">> Setup conda default environment"
@@ -26,7 +30,9 @@ chmod +x /etc/profile.d/conda-env.sh
 
 # Install pip-tools
 echo ">> Install pip-tools"
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 su -l $DEV_CONTAINER_USER /bin/bash -c "source ~/.miniconda3/bin/activate ${CONDA_ENV} && pip install pip-tools"
+echo ">> Installed Pip Tools: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # Setup Environment Variables
 echo ">> Copy Python environment variables"
