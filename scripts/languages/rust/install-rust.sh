d150e2787f4b98be08e0928de4e2031ed844b41e
Pierre LEANDRE <leandre@et.esiea.fr>
2024-06-21 08:43:31 +0800

    Add logs on rust installation

--- scripts/languages/rust/install-rust.sh
+++ scripts/languages/rust/install-rust.sh
@@ -5,6 +5,7 @@ echo "> Installing Rust"
 space_before=$(df --output=avail / --block-size=1 | tail -n 1)
 
 echo ">> Installing Rust Dependencies"
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 apt install -y -qq \
 	curl \
 	build-essential \
@@ -12,10 +13,12 @@ apt install -y -qq \
 	make \
 	wget \
 	tar
+echo ">> Installed Rust Dependencies: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # To Install Rust System wide, we use the standalone installer
 # https://forge.rust-lang.org/infra/other-installation-methods.html#standalone
 echo ">> Downloading Rust Package"
+space_before_cmd=$(df --output=avail / --block-size=1 | tail -n 1)
 wget "https://static.rust-lang.org/dist/rust-${RUST_VERSION}-x86_64-unknown-linux-gnu.tar.gz" -O /tmp/rust-install.tar.gz -q
 tar -C /tmp -xf /tmp/rust-install.tar.gz
 
@@ -23,6 +26,7 @@ echo ">> Running Rust Install"
 /tmp/rust-${RUST_VERSION}-x86_64-unknown-linux-gnu/install.sh
 rm -rf /tmp/rust-${RUST_VERSION}-x86_64-unknown-linux-gnu/
 rm -rf /tmp/rust-install.tar.gz
+echo ">> Installed Rust Tarball: $(numfmt --to=iec $(( space_before_cmd - $(df --output=avail / --block-size=1 | tail -n 1) )))"
 
 # Setup Environment Variables
 echo ">> Copy Rust environment variables"
